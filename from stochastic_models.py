97041b9faba47e927b744f3d68123daab48b3239
Amit Kumar Jha <jha.8@iitj.ac.in>
2024-12-15 11:31:08 +0530

    Initial commit for stochPathSim

---
+++ from stochastic_models.py
@@ -0,0 +1,19 @@
+from stochastic_models.gbm import GeometricBrownianMotion
+from statistics_utils import calculate_statistics, display_statistics, plot_simulation
+
+# Simulate using GBM
+gbm = GeometricBrownianMotion(mu=0.1, sigma=0.2, s0=100, T=1, n=1000, paths=1000)
+time, simulations = gbm.simulate()
+
+# Use the initial value (s0) as the actual value for P_LEVEL
+initial_value = 100
+
+# Calculate statistics
+stats_df = calculate_statistics(simulations, initial_value)
+
+
+# Display the statistics
+display_statistics(stats_df, time)
+
+#Plots the simulated paths
+plot_simulation(time, simulations)
